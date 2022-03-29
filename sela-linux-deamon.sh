8977d4f3d44e242489374931430fbaefa4f6d8e0
tokens <33955492+wattson-coder@users.noreply.github.com>
2022-03-29 14:36:32 +0200

    Create sela-linux-deamon.sh

---
+++ sela-linux-deamon.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev -y
+sudo apt-get install libdb-dev libdb++-dev libminiupnpc-dev -y
+wget "https://github.com/wattson-coder/SelaCoin/blob/main/builds/selacoin-daemon-linux.tar.gz" -O selacoin-daemon-linux.tar.gz
+tar -xzvf selacoin-daemon-linux.tar.gz
+sudo mv selacoind /usr/bin/
+mkdir $HOME/.selacoin
+cd  $HOME/.selacoin/
+wget https://gist.githubusercontent.com/wattson-coder/f3726994a200c3cfebc23894937c4bd0/raw/9002b5ea276e656e10ba0d4c822b238453553d05/selacoin.conf -O selacoin.conf
+selacoind
