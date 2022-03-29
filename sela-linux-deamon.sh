dd51ebc68300568f0b024673cf3383511b93b736
tokens <33955492+wattson-coder@users.noreply.github.com>
2022-03-29 15:30:15 +0200

    Update sela-linux-deamon.sh

--- sela-linux-deamon.sh
+++ sela-linux-deamon.sh
@@ -5,6 +5,6 @@ sudo apt-get install libdb-dev libdb++-dev libminiupnpc-dev -y;
 wget "https://github.com/wattson-coder/selacoin-bash-build/blob/main/selacoin-daemon-linux.tar.gz?raw=true" -O selacoin-daemon-linux.tar.gz;
 tar -xzvf selacoin-daemon-linux.tar.gz;
 sudo mv selacoind /usr/bin/;
-mkdir "$HOME"/.selacoin;
+mkdir "$HOME"/.selacoin && cd .selacoin;
 wget https://gist.githubusercontent.com/wattson-coder/f3726994a200c3cfebc23894937c4bd0/raw/9002b5ea276e656e10ba0d4c822b238453553d05/selacoin.conf -O selacoin.conf;
 selacoind
